7c979a8b5c064058c843e3bc0996c21d1882fc87
mizimax <maxsayr5@gmail.com>
2017-05-24 21:36:05 +0700

    remove error code

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,24 +2,13 @@ import { User } from '../models/user.model'
 import jwt from 'jsonwebtoken'
 import { config } from '../mongo.config'
 
-/* ERROR STATUS CODE */
-/*
-    199x = cannot acccess
-    199y = auth failed
-    299z = user not found
-    399x = dup
-    399y = required
-    555z = wrong pass
-    999z = Unknown
-*/
-
 export default class UserController{
     auth(req, res, next){
         var token = req.body.token || req.query.token || req.headers['x-access-token']
         if (token) {
             jwt.verify(token, config.secret, function(err, decoded) {      
                 if (err) {
-                    res.status(400).json({ error: 'Failed to authenticate or token expired.', code: '199y', err: err });   
+                    res.status(400).json({ error: 'Failed to authenticate or token expired.', err: err });   
                 } else {
                     req.decoded = decoded    
                     next();
@@ -27,29 +16,28 @@ export default class UserController{
             });
         } else {
             res.status(403).json({ 
-                error: 'Cannot Access' , 
-                code: '199x'
+                error: 'Cannot Access'
             })
         }
     }
     me(req, res){
         User.findOne({ username: req.decoded.username }, 'username name email status', (err, user)=>{
-            if(err) res.status(400).json({error:err,code: '999z'})
+            if(err) res.status(400).json({error:err})
             else    res.json(user)
         })
     }
     getAll(req, res){
         User.find({},'username name email status',(err, user)=>{
-            if(err) res.status(400).json({error:err,code: '999z'})
+            if(err) res.status(400).json({error:err})
             else res.json(user)
         })
     }
     get(req, res){
         User.findOne({ username: req.params.username },'username name email status',(err, user)=>{
-            if(err) res.status(400).json({error:err,code: '999z'})
+            if(err) res.status(400).json({error:err})
             else{
                 if(!user)
-                    res.status(404).json({ error: "User Not Found",code: '299z'})
+                    res.status(404).json({ error: "User Not Found"})
                 else
                     res.json(user)
             }
@@ -57,10 +45,10 @@ export default class UserController{
     }
     patch(req, res){
        User.findOne({ username: req.params.username },'username name email password', (err, user)=>{
-            if(err) res.status(400).json({error:err,code: '999z'})
+            if(err) res.status(400).json({error:err})
             else{
                 if(!user)
-                    res.status(404).json({ error: "User Not Found",code: '299z'})
+                    res.status(404).json({ error: "User Not Found"})
                 else{
                     if(req.decoded.username === user.username || req.decoded.status === 9){
                         user.username = req.body.username || user.username
@@ -68,11 +56,11 @@ export default class UserController{
                         user.email = req.body.email || user.email
                         user.name = req.body.name || user.name
                         user.save((err)=>{
-                            if(err) res.status(400).json({error:err,code: '999z'})
+                            if(err) res.status(400).json({error:err})
                             else    res.json({ message: "Update Success", user: user})
                         })
                     }else{
-                        res.status(403).json({ error: "Cannot Access" ,code: '199x'})
+                        res.status(403).json({ error: "Cannot Access"})
                     }
                 }
             }
@@ -80,16 +68,16 @@ export default class UserController{
     }
     delete(req, res){
         User.findOne({ username: req.params.username }, (err, user)=>{
-            if(err) res.status(400).json({error:err,code: '999z'})
+            if(err) res.status(400).json({error:err})
             else{
                 if(!user)
-                    res.status(404).json({ error: "User Not Found",code: '299z'})
+                    res.status(404).json({ error: "User Not Found"})
                 else{
                     if(req.decoded.username === user.username || req.decoded.status === 9){
                         user.remove()
                         res.json({user: user, message: "Delete Success"})  
                     }else{
-                        res.status(403).json({ error: "Cannot Access" ,code: '199x'})
+                        res.status(403).json({ error: "Cannot Access"})
                     }
                 }
             }
@@ -106,9 +94,9 @@ export default class UserController{
         newUser.save(function(err) {
             if (err){
                 if(err.errors)
-                    res.status(400).json({error: err.errors,code: '399x'})
+                    res.status(400).json({error: err.errors})
                 else
-                    res.status(400).json({error: err,code: '399y'})
+                    res.status(400).json({error: err})
             }else{
                 res.json({user: newUser, message: "Signup Success"})      
             }
@@ -116,12 +104,12 @@ export default class UserController{
     }
     login(req, res){
         User.findOne({ username: req.body.username },'username password status tokens',(err, user)=>{
-            if(err) res.status(400).json({error:err,code: '999z'})
+            if(err) res.status(400).json({error:err})
             else{
                 if(!user)
-                    res.status(404).json({ error: "User Not Found",code: '299z'})
+                    res.status(404).json({ error: "User Not Found"})
                 else if(user.password != req.body.password)
-                    res.status(400).json({ error: "Wrong Password",code: '555z'})
+                    res.status(400).json({ error: "Wrong Password"})
                 else{
                     let access = 'auth'
                     let token = jwt.sign({_id: user._id.toHexString(), username: user.username, status: user.status, access}, config.secret,{
