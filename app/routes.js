7e333cc7f221a6e2d871a98a3fe69c061dde543a
mizimax <maxsayr5@gmail.com>
2017-05-24 21:36:33 +0700

    add blog route

--- app/routes.js
+++ app/routes.js
@@ -1,27 +1,29 @@
 import UserController from './controllers/user.controller'
+import BlogController from './controllers/blog.controller'
 
 export default class Route{
     constructor(app){
-        this.myApp = app
+        this.app = app
         this.user = new UserController()
+        this.blog = new BlogController()
     }
     userRoute(){
-        this.myApp.route('/api/v1/users')
+        this.app.route('/api/v1/users')
             .post(this.user.regis)
             .get(this.user.auth,this.user.getAll)
-        this.myApp.post('/api/v1/user/login', this.user.login)
-        this.myApp.get('/api/v1/user/me', this.user.auth, this.user.me)
-        this.myApp.get('/', (req, res)=>{
+        this.app.post('/api/v1/user/login', this.user.login)
+        this.app.get('/api/v1/user/me', this.user.auth, this.user.me)
+        this.app.get('/', (req, res)=>{
             res.json({test: 'test'})
         })
-        this.myApp.route('/api/v1/user/:username')
+        this.app.route('/api/v1/user/:username')
             .get(this.user.auth, this.user.get)
             .patch(this.user.auth, this.user.patch)
             .delete(this.user.auth, this.user.delete)
     }
 
     blogRoute(){
-        //this.myApp.get('/api/v1/blogs', blog.getAll)
+        this.app.get('/api/v1/blogs', this.blog.getAll)
     }
 
 }
