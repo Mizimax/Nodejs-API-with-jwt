0f284e49391aac98841c0a9fce5efb31492b3534
mizimax <maxsayr5@gmail.com>
2017-05-24 15:35:58 +0700

    add blog model

---
+++ app/models/blog.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+var comments = new Schema({
+    comment: { type: String, required: [true, 'Topic Required'] },
+    created_by: { type:String, default: "ผู้ไม่ประสงค์ออกนาม" },
+    created_at: { type: Date, default: Date.now },
+    updated_at: Date
+});
+
+var BlogModel = new Schema({
+    name: { type: String, required: [true, 'Topic Name Required'] },
+    topic: { type: String, required: [true, 'Topic Required']},
+    content: { type: String, required: [true, 'Content Required'] },
+    pic: String,
+    created_by: { type:String, default: "Maxang" },
+    comments: [comments],
+    created_at: { type: Date, default: Date.now },
+    updated_at: Date
+})
+
+export const User = mongoose.model('Blog', BlogModel)
