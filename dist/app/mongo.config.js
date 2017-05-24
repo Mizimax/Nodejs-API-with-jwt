81589dc5247a4d5ebaad5d6a38b2a3d20726b4df
mizimax <maxsayr5@gmail.com>
2017-05-24 15:47:12 +0700

    delete dist folder

--- dist/app/mongo.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-var config = exports.config = {
-    secret: 'MaxMongo',
-    database: 'mongodb://maxang:0847440744@ds159737.mlab.com:59737/maxang'
-};
