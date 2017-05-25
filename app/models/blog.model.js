9caf700fbf725117895816baf1e9818bf83a4424
mizimax <maxsayr5@gmail.com>
2017-05-25 21:45:25 +0700

    add index to blog

--- app/models/blog.model.js
+++ app/models/blog.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 var comments = new Schema({
     comment: { type: String, required: [true, 'Comment Required'] },
-    created_by: { type:String, default: "ผู้ไม่ประสงค์ออกนาม" },
+    created_by: { type:String, default: "Anonymous" },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
 });
@@ -19,5 +19,7 @@ var BlogModel = new Schema({
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
 })
+
+BlogModel.index({"name":1})
 
 export const Blog = mongoose.model('Blog', BlogModel)
